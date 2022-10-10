95f0b85293bf3db6f9191b888734f42eeb582923
ilkerserce <ilker@serceajans.com>
2022-10-10 12:32:59 +0300

    *Sürüm Yükseltmesi* -Veri okuma homeComponent.ts dosyasındaki yerel TS dosyasından posts.service.ts dosyasına atandı.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,22 +1,24 @@
-import { Component} from '@angular/core';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-
-  postUrl = 'http://localhost:3000/posts/?post_type=post';
+  postsUrl = this.postsService.postsUrl;
   posts: any;
 
-  constructor(private httpClient: HttpClient) { 
-    
-    httpClient.get(this.postUrl).subscribe(response => {
-      console.log(this.posts);
-      this.posts = response}
-      )
+  ngAfterViewChecked(): void {
+    this.posts = this.postsService.Posts;
+    console.log('Burası homeComponent.ts');
+    console.log(this.posts);
+    console.log('Burası homeComponent.ts x2');
+    console.log(this.postsService.Posts);
   }
 
+  constructor(private postsService: PostsService) {}
+
+  ngOnInit(): void {}
 }
--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,96 +1,80 @@
-// import { Injectable, Input, Output } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
-// import { getPopperClassPlacement } from '@ng-bootstrap/ng-bootstrap/util/positioning';
+import { Injectable, Input, Output } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class PostsService {
+@Injectable({
+  providedIn: 'root',
+})
+export class PostsService {
+  Posts: any;
+  webPosts: any;
+  contentPosts: any;
+  socialMediaPosts: any;
+  digitalAdsPosts: any;
 
-//   postsUrl = 'http://localhost:3000/posts/?post_type=post';
-//   webUrl = 'http://localhost:3000/posts/?post_category=web';
-//   contentUrl = 'http://localhost:3000/posts/?post_category=content';
-//   socialMediaUrl = 'http://localhost:3000/posts/?post_category=social-media';
-//   digitalAdsUrl = 'http://localhost:3000/posts/?post_category=digital-ads';
+  getAllPosts() {
+    this.httpClient.get(this.postsUrl).subscribe((response) => {
+      this.Posts = response;
+      console.log("Burası postService."); 
+      console.log(this.Posts);
+    });
+  }
+  postsUrl = 'http://localhost:3000/posts/?post_type=post';
+  webUrl = 'http://localhost:3000/posts/?post_category=web';
+  contentUrl = 'http://localhost:3000/posts/?post_category=content';
+  socialMediaUrl = 'http://localhost:3000/posts/?post_category=social-media';
+  digitalAdsUrl = 'http://localhost:3000/posts/?post_category=digital-ads';
 
+  constructor(private httpClient: HttpClient) {
+    const getWebPosts = () => {
+      httpClient.get(this.webUrl).subscribe((response) => {
+        console.log(this.webPosts);
+        this.webPosts = response;
+      });
+    };
+    const getContentPosts = () => {
+      httpClient.get(this.contentUrl).subscribe((response) => {
+        console.log(this.contentPosts);
+        this.contentPosts = response;
+      });
+    };
+    const getSocialMediaPosts = () => {
+      httpClient.get(this.socialMediaUrl).subscribe((response) => {
+        console.log(this.socialMediaPosts);
+        this.socialMediaPosts = response;
+      });
+    };
+    const getDigitalAdsPosts = () => {
+      httpClient.get(this.digitalAdsUrl).subscribe((response) => {
+        console.log(this.digitalAdsPosts);
+        this.digitalAdsPosts = response;
+      });
+    };
 
-//   Posts: any;
-//   webPosts: any;
-//   contentPosts: any;
-//   socialMediaPosts: any;
-//   digitalAdsPosts: any;
+    this.getAllPosts();
+    getWebPosts();
+    getContentPosts();
+    getSocialMediaPosts();
+    getDigitalAdsPosts();
 
-//   // posts: any;
-//   // webPosts: any;
-//   // contentPosts: any;
-//   // socialMediaPosts: any;
-//   // digitalAdsPosts: any;
-
-
-//   constructor(private httpClient: HttpClient) {
-
-//     const getAllPosts = () => {
-//       httpClient.get(this.postsUrl).subscribe(response => {
-//         console.log(this.Posts);
-//         this.Posts = response;
-//       }
-//       )
-//     }
-//     const getWebPosts = () => {
-//       httpClient.get(this.webUrl).subscribe(response => {
-//         console.log(this.webPosts);
-//         this.webPosts = response;
-//       }
-//       )
-//     }
-//     const getContentPosts = () => {
-//       httpClient.get(this.contentUrl).subscribe(response => {
-//         console.log(this.contentPosts);
-//         this.contentPosts = response;
-//       }
-//       )
-//     }
-//     const getSocialMediaPosts = () => {
-//       httpClient.get(this.socialMediaUrl).subscribe(response => {
-//         console.log(this.socialMediaPosts);
-//         this.socialMediaPosts = response;
-//       }
-//       )
-//     }
-//     const getDigitalAdsPosts = () => {
-//       httpClient.get(this.digitalAdsUrl).subscribe(response => {
-//         console.log(this.digitalAdsPosts);
-//         this.digitalAdsPosts = response;
-//       }
-//       )
-//     }
-
-//     getAllPosts();
-//     getWebPosts();
-//     getContentPosts();
-//     getSocialMediaPosts();
-//     getDigitalAdsPosts();
-
-//     // httpClient.get(this.webUrl).subscribe(response => {
-//     //   console.log(this.webPosts);
-//     //   this.webPosts = response;
-//     // }
-//     // )
-//     // httpClient.get(this.contentUrl).subscribe(response => {
-//     //   console.log(this.contentPosts);
-//     //   this.contentPosts = response;
-//     // }
-//     // )
-//     // httpClient.get(this.socialMediaUrl).subscribe(response => {
-//     //   console.log(this.socialMediaPosts);
-//     //   this.socialMediaPosts = response
-//     // }
-//     // )
-//     // httpClient.get(this.digitalAdsUrl).subscribe(response => {
-//     //   console.log(this.digitalAdsPosts);
-//     //   this.digitalAdsPosts = response
-//     // }
-//     // )
-//   }
-// }
-
+    // httpClient.get(this.webUrl).subscribe(response => {
+    //   console.log(this.webPosts);
+    //   this.webPosts = response;
+    // }
+    // )
+    // httpClient.get(this.contentUrl).subscribe(response => {
+    //   console.log(this.contentPosts);
+    //   this.contentPosts = response;
+    // }
+    // )
+    // httpClient.get(this.socialMediaUrl).subscribe(response => {
+    //   console.log(this.socialMediaPosts);
+    //   this.socialMediaPosts = response
+    // }
+    // )
+    // httpClient.get(this.digitalAdsUrl).subscribe(response => {
+    //   console.log(this.digitalAdsPosts);
+    //   this.digitalAdsPosts = response
+    // }
+    // )
+  }
+}
